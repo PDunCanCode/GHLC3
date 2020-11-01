0c7a3ab57cc7982dabbef27874a225b20c019c5c
pDunCanCode <chesiredarling@gmail.com>
2020-11-01 09:49:30 -0800

    add userModel && productModel

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 import connectDB from './config/db.js';
+import colors from 'colors';
 
 dotenv.config()
 
@@ -23,5 +24,6 @@ app.get('/api/products/:id', (req, res) => {
 
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
 )
