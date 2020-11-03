059cd58afb4068cc4fedb4ca10da2590db824533
pDunCanCode <chesiredarling@gmail.com>
2020-11-02 20:09:29 -0800

    add redux & store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./bootstrap.min.css";
 import "./index.css";
+import { Provider } from 'react-redux';
+import store from './store'
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  
+  <Provider>
     <App />,
+  </Provider>,
+    
   
   document.getElementById("root")
 );
