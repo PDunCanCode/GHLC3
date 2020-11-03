0b13649c3c1d2012b0aea41e23ae3fc4887aad90
pDunCanCode <chesiredarling@gmail.com>
2020-11-03 08:19:11 -0800

    rewrite homeScreen with useDispatch

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler'
+import express from "express";
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
@@ -8,25 +8,29 @@ const router = express.Router();
 //@route GET /api/products
 //@access Public Route
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc Fetch Single Products
 //@route GET /api/products/:id
 //@access Public Route
 router.get(
-    '/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(product){
-    res.json(product)
+    if (product) {
+      res.json(product);
     } else {
-        res.status(404)
-        throw new Error('Product not Found')
+      res.status(404);
+      throw new Error("Product not Found");
     }
-}))
+  })
+);
 
-
-export default router
+export default router;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productsActions";
 
-const HomeScreen = ({ match }) => {
-    const [products, setProducts] = useState([])
-    
-    
-    
-    useEffect(() =>{
-       const fetchProducts = async() =>{
-           const res = await axios.get('/api/products')
+const HomeScreen = ({}) => {
+  const dispatch = useDispatch();
 
-           setProducts(data) 
-       }
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-       fetchProducts()
-    }, [])
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (    
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
+  const products = [];
 
-export default HomeScreen
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>LOADING...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
