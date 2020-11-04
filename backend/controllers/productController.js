3831dad8e4cd239a71141be8d52b151b44230722
pDunCanCode <chesiredarling@gmail.com>
2020-11-04 15:17:09 -0800

    clean up productRoutes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,28 @@
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
+
+// @desc Fetch all Products
+//@route GET /api/products
+//@access Public Route
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+
+  res.json(products);
+});
+
+// @desc Fetch Single Products
+//@route GET /api/products/:id
+//@access Public Route
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
+
+export { getProductById, getProductById };
